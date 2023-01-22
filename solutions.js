f08bc4f143be12e7924217b1fcb8e9c805ed6028
David <davidbanker22@yahoo.com>
2023-01-22 17:46:48 -0600

    finished conditionals self test and practice assessment

--- solutions.js
+++ solutions.js
@@ -21,3 +21,91 @@
  * concat
  */
 
+function isTrue(x) {
+    if(x === true) {
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log(isTrue(""));
+
+function isFalse(x) {
+    if(x === false) {
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log(isFalse([1,2]));
+
+function not(x) {
+    let y = Boolean(x);
+    if(y == true) {
+        return false;
+    } else {
+        return true;
+    }
+}
+console.log(not(false));
+
+function addOne(x) {
+    let y = parseInt(x);
+    if(typeof x === "number") {
+        return x + 1;
+    } else if(typeof x === "string") {
+        return y + 1;
+    } else {
+        return y + 1;
+    }
+}
+console.log(addOne(true));
+
+function isEven(x) {
+    let y = parseInt(x);
+    if(typeof y === "number" && y % 2 == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log(isEven("banana"));
+
+function isIdentical(x, y) {
+    if(x === y) {
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log(isIdentical("javascript", "java"));
+
+function isEqual(x, y) {
+    if(x == y) {
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log(isEqual("java", "javascript"));
+
+function or(x, y) {
+    return x || y;
+}
+console.log(or("hello", "world"));
+
+function and(x, y) {
+    return x && y;
+}
+console.log(and("hello" , "world"));
+
+function concat(x, y) {
+    if(typeof x !== "string" && typeof y !== "string") {
+        let yChange = y.toString();
+        let xChange = x.toString();
+        return xChange.concat(yChange);
+    } else {
+        return x.concat(y);
+    }
+}
+console.log(concat("code", undefined))
